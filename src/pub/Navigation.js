7318b7cf83f0bf330199bae85e65bb13f41bfec0
Shin Songhee <fesa02@gmail.com>
2022-03-31 17:33:45 +0900

    Navigation

--- src/pub/Navigation.js
+++ src/pub/Navigation.js
@@ -1,20 +1,59 @@
+import NavMember from "@/assets/images/img-nav-member.png";
+
 const Navigation = () => {
   return (
     <>
-      <nav className="nav-wrap">
+      <nav className="navWrap">
         <header>
-          <img src={""} alt={""} />
+          <img src={NavMember} alt={""} />
           <p>홍길동님</p>
+          {/* <button>로그인하기</button> */}
         </header>
-        <ul>
+        <ul className="navList">
           <li>
-            <p></p>
+            <p className="first gaa">Golf AI Analysis</p>
             <ul>
-              <li></li>
+              <li>
+                <button>목록보기</button>
+              </li>
+              <li>
+                <button>보조선 그리기</button>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <p className="first swing">프로스윙</p>
+            <ul>
+              <li>
+                <button>프로 리스트</button>
+              </li>
+              <li>
+                <button>프로스윙 목록</button>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <button className="first compare">스윙영상 비교</button>
+          </li>
+          <li>
+            <button className="first rank">랭킹</button>
+          </li>
+          <li>
+            <p className="first commu">커뮤니티</p>
+            <ul>
+              <li>
+                <button>1:1문의</button>
+              </li>
+              <li>
+                <button>커뮤니티</button>
+              </li>
             </ul>
           </li>
         </ul>
+        <button className="navSetting">Setting</button>
+        <button className="navClose">Close</button>
       </nav>
+      <div className="dim"></div>
     </>
   );
 };
