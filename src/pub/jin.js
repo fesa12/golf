312012725b392d5e14ef2b0e09a6ba2f17d4d26e
Jin Eunju <jin@fidems8.com>
2022-04-05 13:56:03 +0900

    test

--- src/pub/jin.js
+++ src/pub/jin.js
@@ -1,9 +1,20 @@
 import "./scss/jin.scss";
 const jin = () => {
     return (
-    <div className="test">
-        <h3>hi</h3>
-        화요일 화요일
+    <div className="test-wrap">
+        <header>
+            <div class="logo">logo</div>
+            <nav>
+                <ul>
+                    <li>메뉴</li>
+                    <li>메뉴</li>
+                    <li>메뉴</li>
+                    <li>메뉴</li>
+                    <li>메뉴</li>
+                </ul>
+            </nav>
+        </header>
+        <div class="content"></div>
     </div>
     );
 };
