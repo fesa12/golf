5a13b5c6f9349f55f61d1a3b99e031bacff7c8a1
Jin Eunju <jin@fidems8.com>
2022-04-05 18:03:58 +0900

    test

--- src/pub/jin.js
+++ src/pub/jin.js
@@ -21,10 +21,20 @@ const jin = () => {
                     <input id="pw-input" type="password" placeholder="비밀번호"></input>
                 </div>
             </fieldset>
+            <div class="user-box">
+                <div class="keep-check-box">
+                    <input type="radio" id="keep-check"></input>
+                    <label for="keep-check">자동 로그인</label>
+                </div>
+                <div class="find-box">
+                    <a href="#n">비밀번호 찾기</a>
+                    <a href="#n">회원가입</a>
+                </div>
+            </div>
+            <button type="button">로그인</button>     
         </div>
 
     </div>
     );
 };
-
 export default jin;
