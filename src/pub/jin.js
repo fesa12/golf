d45396b25747fa6d218d6f20484778b1fe8d15bb
Jin Eunju <jin@fidems8.com>
2022-04-05 13:17:11 +0900

    테스트

---
+++ src/pub/jin.js
@@ -0,0 +1,13 @@
+import './assets/scss/jin.scss';
+
+function jin() {
+  return (
+    <div className="test">
+        <h3>
+            hi
+        </h3>
+    </div>
+  );
+}
+
+export default jin;
