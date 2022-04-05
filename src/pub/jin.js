2e660104acb235e38f0044b44d87380801ee4c13
Jin Eunju <jin@fidems8.com>
2022-04-05 14:28:20 +0900

    테스트 ~!

--- src/pub/jin.js
+++ src/pub/jin.js
@@ -3,21 +3,26 @@ const jin = () => {
     return (
     <div className="test-wrap">
         <header>
-            <div class="logo">logo</div>
+            <h2>로그인</h2>
             <nav>
-                <ul>
-                    <li>메뉴</li>
-                    <li>메뉴</li>
-                    <li>메뉴</li>
-                    <li>메뉴</li>
-                    <li>메뉴</li>
-                </ul>
+                <span></span>
+                <span></span>
+                <span></span>
             </nav>
         </header>
         <div class="content">
-
+            <fieldset>
+                <div class="mailBox">
+                    <label for="mail-input">이메일</label>
+                    <input id="mail-input" type="text" placeholder="이메일"></input>
+                </div>
+                <div class="pwBox">
+                    <label for="pw-input">비밀번호</label>
+                    <input id="pw-input" type="password" placeholder="비밀번호"></input>
+                </div>
+            </fieldset>
         </div>
-        
+
     </div>
     );
 };
